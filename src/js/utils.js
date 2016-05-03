9e66d49fa0c3967951319979fe1d0c18a7dd699e
triad <flores.leonardo@gmail.com>
2016-05-03 09:59:07 -0300

    Examples updates and corrections

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,5 +1,6 @@
 /**
 jQuery extends replacemement
+TODO: Resolve jQuery conflict
 */
 //Object.prototype.extends = function (out) {
 //    out = out || {};
@@ -21,6 +22,31 @@ jQuery extends replacemement
 //    }
 //    return this;
 //};
+
+/**
+jQuery extend replacement
+*/
+var deepExtend = function (out) {
+    out = out || {};
+
+    for (var i = 1; i < arguments.length; i++) {
+        var obj = arguments[i];
+        if (!obj) {
+            continue;
+        }
+
+        for (var key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                if (typeof obj[key] === 'object')
+                    out[key] = deepExtend(out[key], obj[key]);
+                else
+                    out[key] = obj[key];
+            }
+        }
+    }
+    return out;
+};
+
 
 if (typeof String.replaceParams === 'undefined') {
     /**
--- tests/utils.tests.js
+++ tests/utils.tests.js
@@ -1,25 +1,3 @@
-var deepExtend = function (out) {
-    out = out || {};
-
-    for (var i = 1; i < arguments.length; i++) {
-        var obj = arguments[i];
-        if (!obj) {
-            continue;
-        }
-
-        for (var key in obj) {
-            if (obj.hasOwnProperty(key)) {
-                if (typeof obj[key] === 'object')
-                    out[key] = deepExtend(out[key], obj[key]);
-                else
-                    out[key] = obj[key];
-            }
-        }
-    }
-    return out;
-};
-
-
 function getContainer(id) {
     var $child = $("<div id=\"" + id + "\"></div>");
     $child.css("height", 300);
@@ -32,7 +10,7 @@ function getContainer(id) {
 
 
 describe('objects tests', function () {
-    it('should be loaded', function () {
+    it('check merge data from deepExtend', function () {
         var _defaults = {
             sticky: false,
             height: 600,
@@ -64,33 +42,30 @@ describe('objects tests', function () {
         });
         //var config = deepExtend(_defaults, _config);
         //var config = bubbles.extend(_defaults, _config);
-        var config = _defaults.extends(_config);
+        var config = deepExtend(_defaults, _config);
         expect(config.sticky).toBe(false);
         expect(config.minRadius).toBe(10);
         expect(config.width).toBe(800);
         expect(config.height).toBe(600);
         expect(config.data.label.autofit).toBe(true);
         expect(config.data.tooltip.template).toBe("lalala");
-
-
-
     });
 
     it("text split test", function () {
         //43334556667
-        //FIGNONI, BRUNO
+        //FERNANDEZ, CARLOS
         //Size: 345654636
         //Perc: 45%
-        var _splited = "43334556667 - FIGNONI, BRUNO\nSize: 345654636\nPerc: 45%".splitMultiple();
+        var _splited = "43334556667 - FERNANDEZ, CARLOS\nSize: 345654636\nPerc: 45%".splitMultiple();
         expect(_splited.length).toBe(4);
 
-        var _other = "43334556667 - FIGNONI, BRUNO - Size: 3 - Perc: 45%".splitMultiple();
+        var _other = "43334556667 - FERNANDEZ, CARLOS - Size: 3 - Perc: 45%".splitMultiple();
         expect(_other.length).toBe(4);
 
-        var _other = "43334556667 - FIGNONI, BRUNO\nSize: 3 - Perc: 45%".splitMultiple();
+        var _other = "43334556667 - FERNANDEZ, CARLOS\nSize: 3 - Perc: 45%".splitMultiple();
         expect(_other.length).toBe(4);
 
-        var _other = "43334556667 - FIGNONI, BRUNO\nSize: 3\nPerc: 45%".splitMultiple();
+        var _other = "43334556667 - FERNANDEZ, CARLOS\nSize: 3\nPerc: 45%".splitMultiple();
         expect(_other.length).toBe(4);
     });
 });
