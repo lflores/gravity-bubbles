b0a3720cafb0c469b8c74bb494774d026f871ba9
Triad <leonardo.flores@overactive.com>
2019-11-07 18:45:02 -0300

    Travis-Ci build

--- karma.conf.js
+++ karma.conf.js
@@ -1,58 +1,82 @@
-module.exports = function (config) {
+// karma-browserify karma.conf.js
+
+// Karma configuration
+// Generated on Sat Dec 16 2017 16:32:55 GMT+0600 (Bangladesh Standard Time)
+module.exports = function(config) {
     config.set({
 
-        // base path, that will be used to resolve files and exclude
-        basePath: './',
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
+
 
         // frameworks to use
-        frameworks: ['jasmine'],
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine','browserify'],
+
+
 
         // list of files / patterns to load in the browser
         files: [
-            {
-                pattern: './tests/*.html',
-                watched: false,
-                served: true,
-                included: false
-			 },
-			'./examples/css/basic-styles.css',
-			'./src/css/gravity-bubbles.css',
-            './src/js/utils.js',
-            './src/js/custom-tooltip.js',
-            './src/js/gravity-bubbles.js',
-			'./tests/libs/*.js',
-			'./tests/*.tests.js',
-            //'./tests/gravity.text.tests.js'
+            // 'test/**/*.js',
 
+            'test/**/*.[sS]pec.js',
+            // 'dist/*.js',
         ],
+
 
         // list of files to exclude
         exclude: [
+
         ],
 
+
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            'dist/*.js': ['coverage'],
+            'test/**/*.[sS]pec.js': ['browserify'],
+        },
         // test results reporter to use
-        reporters: ['progress'],
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress', 'coverage'],
+
 
         // web server port
         port: 9876,
 
+
         // enable / disable colors in the output (reporters and logs)
         colors: true,
 
+
         // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
         logLevel: config.LOG_INFO,
+
 
         // enable / disable watching file and executing tests whenever any file changes
         autoWatch: true,
 
-        // Start these browsers
-        browsers: ['PhantomJS'],
 
-        // If browser does not capture in given timeout [ms], kill it
-        captureTimeout: 60000,
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['Chrome'],
+
 
         // Continuous Integration mode
-        // if true, it capture browsers, run tests and exit
-        singleRun: false
-    });
-};
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: false,
+
+        // Concurrency level
+        // how many browser should be started simultaneous
+        concurrency: Infinity,
+        coverageReporter: {
+            dir: './coverage',
+            reporters: [
+                { type: 'lcov', subdir: '.' },
+                { type: 'text-summary' }
+            ]
+        }
+    })
+}
